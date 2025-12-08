c99d4a949eb4ad8ca76101259bc1aedff1a048aa
김준표juunpy0 <junpyokim0106@gmail.com>
2025-12-08 18:00:43 +0900

    feat: 로그안 회원가입 버튼 추가

--- GAMI iOS/ContentView.swift
+++ GAMI iOS/ContentView.swift
@@ -6,7 +6,7 @@ struct ContentView: View {
     var body: some View {
         NavigationStack {
             ZStack {
-             a   VStack(alignment: .center) {
+              VStack(alignment: .center) {
                     Image("logo")
                         .resizable()
                         .scaledToFit()
--- GAMI iOS/StartView.swift
+++ GAMI iOS/StartView.swift
@@ -9,12 +9,13 @@ import SwiftUI
 
 struct StartView: View {
     var body: some View {
-        VStack(alignment: .center){
+        VStack(alignment: .center, spacing: 0){
             Image("logo")
                 .resizable()
                 .scaledToFit()
                 .frame(width: 160, height: 128)
                 .padding(.top, 202)
+            
             
             Text("멘토와 멘티를 연결하는 맞춤형 멘토링 서비스")
             .font(
@@ -23,9 +24,61 @@ struct StartView: View {
             )
             .multilineTextAlignment(.center)
             .frame(width: 194, height: 48, alignment: .center)
+    
             .padding(.top, 83)
+        
             Spacer()
-        }
+            
+            Text("로그인")
+                .font(
+                Font.custom("Pretendard", size: 18)
+                .weight(.bold)
+                )
+                .foregroundColor(.white)
+                .padding(.horizontal, 135)
+                .padding(.vertical, 14)
+                .background(Color(red: 0.75, green: 0.66, blue: 1, opacity: 1))
+            .cornerRadius(12)
+            .padding(.bottom, 15)
+            
+            HStack(alignment: .center, spacing: 0) {
+                
+                Rectangle()
+                .foregroundColor(Color(red: 0.72, green: 0.74, blue: 0.78))
+                .frame(width: 128.00296, height: 1)
+                .background(Color(red: 0.72, green: 0.74, blue: 0.78))
+                .padding(.trailing, 12)
+                .padding(.leading, 24)
+                
+                Text("처음이라면?")
+                    .font(Font.custom("Pretendard", size: 14)
+                        .weight(.medium))
+                    .foregroundColor(.gray)
+
+                
+                Rectangle()
+                    .foregroundColor(Color(red: 0.72, green: 0.74, blue: 0.78))
+                .frame(width: 128.00296, height: 1)
+                .padding(.trailing, 12)
+                .padding(.leading, 24)
+            }
+            .padding(.bottom, 10)
+            
+            Text("회원가입")
+                .font(
+                Font.custom("Pretendard", size: 18)
+                .weight(.bold)
+                )
+                .foregroundColor(.white)
+                .padding(.horizontal, 135)
+                .padding(.vertical, 14)
+                .background(Color(red: 0.45, green: 0.66, blue: 1, ))
+            .cornerRadius(12)
+            .padding(.bottom, 32)
+            }
+            
+
+        
         .navigationBarBackButtonHidden(true)
         
     }

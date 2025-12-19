b3f10ebc35285ee8985b0da7f1787a2e8e103a27
김준표juunpy0 <junpyokim0106@gmail.com>
2025-12-19 12:17:52 +0900

    feat: set up Mentor feature structure

---
+++ GAMI iOS/Views/Mantor/FindMantorView.swift
@@ -0,0 +1,18 @@
+//
+//  FindMantorView.swift
+//  GAMI iOS
+//
+//  Created by 김준표 on 12/19/25.
+//
+
+import SwiftUI
+
+struct FindMantorView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FindMantorView()
+}

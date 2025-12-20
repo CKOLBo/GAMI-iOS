35a564694924f6a2687e90d5fc9f380f19d02bf5
김준표juunpy0 <junpyokim0106@gmail.com>
2025-12-21 00:17:52 +0900

    feat: d MentorEmptyView UI

--- GAMI iOS/Views/Mantor/MentorEmptyView.swift
+++ GAMI iOS/Views/Mantor/MentorEmptyView.swift
@@ -9,7 +9,27 @@ import SwiftUI
 
 struct MentorEmptyView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(spacing: 18) {
+            Image("find 1")
+                .resizable()
+                .scaledToFit()
+                .frame(width: 140, height: 140)
+                .foregroundColor(Color("Gray2"))
+
+            VStack(spacing: 6) {
+                Text("일치하는 멘토를 찾지 못했어요")
+                    .font(.custom("Pretendard-Bold", size: 16))
+                    .foregroundColor(Color("Gray1"))
+                
+
+                Text("멘토를 추천 받아 볼까요?")
+                    .font(.custom("Pretendard-Bold", size: 16))
+                    .foregroundColor(Color("Gray1"))
+            }
+            .multilineTextAlignment(.center)
+        }
+        .frame(maxWidth: .infinity)
+        .padding(.top, 54)
     }
 }
 
